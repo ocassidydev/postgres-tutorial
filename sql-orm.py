9e241939c8408cc641a96365a36e85625ea7088d
ocassidydev <ocassidy@tcd.ie>
2022-12-31 20:13:37 +0000

    Finished lesson

--- sql-orm.py
+++ sql-orm.py
@@ -53,19 +53,35 @@ base.metadata.create_all(db)
 #     print(artist.ArtistId, artist.Name, sep=" | ")
 
 # Query 2 - select only the "Name" column from the "Artist" table
-artists = session.query(Artist)
-for artist in artists:
-    print(artist.Name)
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
 
 # Query 3 - select only "Queen " from the "Artist" tab
-# select_query = artist_table.select().where(
-#     artist_table.c.Name == "Queen")
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
-# select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
 
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-# select_query = album_table.select().where(album_table.c.ArtistId == 51)
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
 
 # Query 6 - select all tracks where the composer is "Queen" from the
 # "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | ")
